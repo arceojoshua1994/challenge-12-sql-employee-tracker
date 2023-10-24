33e5854a6c24d84c5e50db64e99d520725f1efff
arceojoshua94 <arceojoshua94@outlook.com>
2023-10-24 19:05:20 -0400

    create database

---
+++ seeds.sql
@@ -0,0 +1,11 @@
+INSERT INTO department (id, name) VALUES
+  (1, 'Engineering'),
+  (2, 'Sales'),
+  (3, 'Marketing');
+
+INSERT INTO role (id, title, salary, department_id) VALUES
+  (1, 'Software Engineer', 80000, 1),
+  (2, 'Sales Manager', 100000, 2),
+  (3, 'Marketing Coordinator', 60000, 3);
+
+-- Add more data as needed
