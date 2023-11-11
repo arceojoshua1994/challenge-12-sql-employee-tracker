6ebf597fc02e1a0e9ce043d288cf9b6744b75bab
arceojoshua94 <arceojoshua94@outlook.com>
2023-11-10 22:11:16 -0500

    add to seeds

--- seeds.sql
+++ seeds.sql
@@ -1,15 +1,32 @@
-USE company_db;
+USE employee_db;
 
--- Departments
-INSERT INTO department (name) VALUES ('Sales');
-INSERT INTO department (name) VALUES ('Engineering');
-INSERT INTO department (name) VALUES ('HR');
+INSERT INTO department (name)
+VALUES ("Sales"),
+("Engineering"),
+("Finance"),
+("Legal");
 
--- Roles
-INSERT INTO role (title, salary, department_id) VALUES ('Sales Executive', 50000, 1);
-INSERT INTO role (title, salary, department_id) VALUES ('Software Engineer', 80000, 2);
-INSERT INTO role (title, salary, department_id) VALUES ('HR Manager', 60000, 3);
+INSERT INTO role (title,salary,department_id)
+VALUES ("Sales Lead", 100000, 1),
+("Sales Person", 80000, 1),
+("Lead Engineer", 150000, 2),
+("Software Engineer", 120000, 2),
+("Account Manager", 160000, 3),
+("Accountant", 125000, 3),
+("Legal Team Lead", 250000, 4),
+("Lawyer", 190000, 4);
 
--- Employees
-INSERT INTO employee (first_name, last_name, role_id) VALUES ('John', 'Doe', 1);
-INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('Jane', 'Smith', 2, 1);
+INSERT INTO employee (first_name,last_name,role_id)
+VALUES ("John", "Doe", 1),
+("Mike","Chan", 2),
+("Ashley","Rodriguez", 3),
+("Kevin","Tupik", 4),
+("Kunal","Singh", 5),
+("Malia","Brown", 6),
+("Sarah","Lourd", 7),
+("Tom","Allen", 8);
+
+UPDATE employee SET manager_id = 1 WHERE id = 2;
+UPDATE employee SET manager_id = 3 WHERE id = 4;
+UPDATE employee SET manager_id = 5 WHERE id = 6;
+UPDATE employee SET manager_id = 7 WHERE id = 8;
